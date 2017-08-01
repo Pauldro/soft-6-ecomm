8915364c786170defa5f81f679c9aa7232c5cde5
bbullemer <barbara.bullemer@gmail.com>
2017-08-01 16:40:31 -0500

    Prodcut Page
    Started product page

--- site/templates/product-page.php
+++ site/templates/product-page.php
@@ -1,10 +1,21 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-</head>
+<?php include('./_head.php'); ?>
 
-<body>
-</body>
-</html>
+	<div class="container page">
+		<div class="row">
+      <div class="col-md-1"></div>
+		  <div class="col-md-5">
+		    <h1>Paint > Colors > <?php echo $page->product_title; ?></h1>
+        <img class="img-responsive" src="<?php $page->product_image->height(400)->url; ?>" alt="">
+        <p><?php echo $page->product_description; ?></p>
+		  </div>
+      <div class="col-md-5">
+        <h2>Features</h2>
+        <p><?php echo $page->product_features; ?></p>
+        <h2>Specifications</h2>
+        <p><?php echo $page->product_specifications; ?></p>
+      </div>
+      <div class="col-md-1"></div>
+		</div>
+	</div>
+
+<?php include('./_foot.php'); // include footer markup ?>
