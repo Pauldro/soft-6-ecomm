c9561e0c7db48c45a0dbd0585a0644659bbd7e65
bbullemer <barbara.bullemer@gmail.com>
2017-08-18 17:03:31 -0500

    8/18/2017 v. 3
    Created cart and made repsonsive

--- site/templates/cart.php
+++ site/templates/cart.php
@@ -2,102 +2,89 @@
 
 <div class="container page">
     <div class="cart-container">
-        <!-- <div class="table-responsive">
-            <h1>Your Cart</h1>
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Product</th>
-                        <th>Quantity</th>
-                        <th class="text-center">Price</th>
-                        <th class="text-center">Total</th>
-                        <th> </th>
-                    </tr>
-                </thead>
-                <tbody>
 
-                      <?php
-                        $cartdetails = get_cart(session_id(), false);
-                        foreach ($cartdetails as $cartdetail) :
-                      ?>
-                      <tr>
-                        <td class="col-sm-8 col-md-6">
-                          <div class="media">
-                              <a class="pull-left" href="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->url; ?>">
-                                <img class="media-object" src="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->product_image->url; ?>" style="width: 72px; height: 72px;" alt="product image">
-                              </a>
-                              <div class="media-body">
-                                  <h4 class="media-heading"><a href="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->url; ?>"><?php echo $pages->get("product_model=".$cartdetail['itemid'])->title; ?></a></h4>
-                                  <span>Model: <?php echo $cartdetail['itemid']; ?></span>
-                              </div>
-                          </div>
-                        </td>
+        <h1>Your Cart</h1>
+        <hr class="title-divider">
+        <div class="column-labels row">
+            <label class="col-md-2">Image</label>
+            <label class="col-md-5">Product</label>
+            <label class="col-md-1">Price</label>
+            <label class="col-md-1">Quantity</label>
+            <label class="col-md-1">Total</label>
+            <label class="col-md-2"><p></p></label>
+        </div>
+        <hr class="label-divider">
 
-                        <td class="col-sm-1 col-md-1" style="text-align: center">
-                            <input type="text" class="form-control qty" value="<?php echo $cartdetail['qty']; ?>">
-                        </td>
-                          <td class="col-sm-1 col-md-1 text-center"><strong>$<?php echo $cartdetail['price']; ?></strong></td>
-                          <td class="col-sm-1 col-md-1 text-center"><strong>$<?php echo $cartdetail['amount']; ?></strong></td>
-                          <td class="col-sm-1 col-md-1">
+        <?php
+          $cartdetails = get_cart(session_id(), false);
+          foreach ($cartdetails as $cartdetail) :
+        ?>
 
-                              <form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
-                                <input type="hidden" name="action" value="update-line">
-                                <input type="hidden" name="linenbr" value="<?php echo $cartdetail['recordno']; ?>">
-                                <input type="hidden" name="qty" value="<?php echo $cartdetail['qty']; ?>">
-                                <input type="hidden" name="page" value="<?php echo $page->url; ?>">
-                                <button type="submit" class="btn update">Update</button>
-                              </form>
+        <div class="product row">
 
-                              <form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
-                                <input type="hidden" name="action" value="remove-line">
-                                <input type="hidden" name="linenbr" value="<?php echo $cartdetail['recordno']; ?>">
-                                <input type="hidden" name="page" value="<?php echo $page->url; ?>">
-                                <button type="submit" class="btn remove">Remove</button>
-                              </form>
+            <a href="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->url; ?>">
+                <img class="col-md-2 img-responsive" src="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->product_image->height(100)->url; ?>" alt="">
+            </a>
 
-                        </td>
-                      </tr>
-                    <?php
-                      endforeach;
-                    ?>
+            <div class="col-md-5">
+                <h4 class="">
+                    <a href="<?php echo $pages->get("product_model=".$cartdetail['itemid'])->url; ?>"><?php echo $pages->get("product_model=".$cartdetail['itemid'])->title; ?></a>
+                </h4>
+                <p class="">Model: <?php echo $cartdetail['itemid']; ?></p>
+            </div>
 
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td>   </td>
-                        <td>   </td>
-                        <td>   </td>
-                        <td>
-                          <h5>Subtotal<br>
-                            Estimated shipping
-                          </h5>
-                          <h3 class="green">Total</h3>
-                        </td>
-                        <td class="text-right">
-                          <h5><strong>1.00<br>
-                            1.00</strong>
-                          </h5>
-                          <h3 class="green">$1.00</h3>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>   </td>
-                        <td>   </td>
-                        <td>   </td>
-                        <td>
-                          <button type="button" class="btn btn-default">
-                              <span class="glyphicon glyphicon-shopping-cart"></span> Continue Shopping
-                          </button>
-                        </td>
-                        <td>
-                          <button type="button" class="btn checkout">Checkout</button>
-                        </td>
-                    </tr>
-                </tfoot>
-            </table>
-        </div> -->
+            <p class="col-md-1 product-price">
+                $<?php echo $cartdetail['price']; ?>
+            </p>
 
-        
+            <div class="col-md-1 product-quantity">
+                <input type="text" class="form-control input-sm auto-width" size="4" name="qty" value="<?php echo $cartdetail['qty']; ?>">
+            </div>
+
+            <h4 class="col-md-1 product-total green">
+                $<?php echo $cartdetail['amount']; ?>
+            </h4>
+
+            <div class="col-md-2 btn-form">
+                <form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
+                  <input type="hidden" name="action" value="update-line">
+                  <input type="hidden" name="linenbr" value="<?php echo $cartdetail['recordno']; ?>">
+                  <input type="hidden" name="qty" value="<?php echo $cartdetail['qty']; ?>">
+                  <input type="hidden" name="page" value="<?php echo $page->url; ?>">
+                  <button type="submit" class="btn update">Update</button>
+                </form>
+                <form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
+                  <input type="hidden" name="action" value="remove-line">
+                  <input type="hidden" name="linenbr" value="<?php echo $cartdetail['recordno']; ?>">
+                  <input type="hidden" name="page" value="<?php echo $page->url; ?>">
+                  <button type="submit" class="btn remove">Remove</button>
+                </form>
+            </div>
+        </div>
+        <hr>
+        <?php
+          endforeach;
+        ?>
+
+        <div class="totals row">
+            <div class="col-md-2 right">
+                <p class="totals-value" id="cart-subtotal">$10.00</p>
+                <p class="totals-value" id="cart-shipping">$5.00</p>
+                <h3 class="totals-value green cart-total" id="cart-subtotal">$15.00</h3>
+            </div>
+            <div class="col-md-2 right">
+                <p>Subtotal</p>
+                <p>Shipping</p>
+                <h3 class="cart-total">Total</h3>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3 right">
+                <button class="btn checkout">Checkout</button>
+            </div>
+        </div>
+
     </div>
 </div>
 
