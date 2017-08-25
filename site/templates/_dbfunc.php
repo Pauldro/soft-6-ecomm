2198ad602127b9728d949119be77f17ffc4a7d30
Paul Gomez <pauldro952@outlook.com>
2017-08-25 15:30:36 -0500

    2

--- site/templates/_dbfunc.php
+++ site/templates/_dbfunc.php
@@ -28,6 +28,8 @@
 		}
 	}
 
+
+
 	function getcartmaxrecord($sessionid, $debug) {
 		$sql = wire('database')->prepare("SELECT MAX(recordno) FROM cart WHERE sessionid = :sessionid");
 		$switching = array(':sessionid' => $sessionid); $withquotes = array(true);
@@ -39,92 +41,10 @@
 		}
 	}
 
-	function family_exists($familyID, $debug) {
-		$sql = wire('database')->prepare("SELECT COUNT(*) FROM family WHERE famID = :famID");
-		$switching = array(':famID' => $familyID); $withquotes = array(true);
-		if ($debug) {
-			return returnsqlquery($sql->queryString, $switching, $withquotes);
-		} else {
-			$sql->execute($switching);
-			return $sql->fetchColumn();
-		}
-	}
 
-	function item_exists($itemID, $debug) {
-		$sql = wire('database')->prepare("SELECT COUNT(*) FROM im WHERE itemid = :itemID");
-		$switching = array(':itemID' => $itemID); $withquotes = array(true);
-		if ($debug) {
-			return returnsqlquery($sql->queryString, $switching, $withquotes);
-		} else {
-			$sql->execute($switching);
-			return $sql->fetchColumn();
-		}
-	}
-
-	function get_familymaxrecnbr($debug) {
-		$sql = wire('database')->prepare("SELECT MAX(recno) FROM family");
-		if ($debug) {
-			return $sql->queryString;
-		} else {
-			$sql->execute();
-			return $sql->fetchColumn();
-		}
-	}
-
-	function get_itemim($itemID, $debug) {
-		$sql = wire('database')->prepare("SELECT * FROM im WHERE itemid = :itemID");
-		$switching = array(':itemID' => $itemID); $withquotes = array(true);
-
-		if ($debug) {
-			return returnsqlquery($sql->queryString, $switching, $withquotes);
-		} else {
-			$sql->execute($switching);
-			return $sql->fetch(PDO::FETCH_ASSOC);
-		}
-	}
-
-	function get_immaxrecnbr($debug) {
-		$sql = wire('database')->prepare("SELECT MAX(recordno) FROM im");
-		if ($debug) {
-			return $sql->queryString;
-		} else {
-			$sql->execute();
-			return $sql->fetchColumn();
-		}
-	}
 	
-	function insert_family($family, $debug) {
-		$q = new atk4\dsql\Query();
-		$q->mode('insert')->table('family');
-		$q = makedsqlqueryinsert($q, $family);
-		$sql = wire('database')->prepare($q->render());
-		if ($debug) {
-			return $q->getDebugQuery();
-		} else {
-			$sql->execute($q->params);
-			return array('sql' => $q->getDebugQuery(), 'insertedid' => wire('database')->lastInsertId());
-		}
-	}
-
-	function insert_product($product, $debug) {
-		$q = new atk4\dsql\Query();
-		$q->mode('insert')->table('im');
-		$q = makedsqlqueryinsert($q, $product);
-		$sql = wire('database')->prepare($q->render());
-		if ($debug) {
-			return $q->getDebugQuery();
-		} else {
-			$sql->execute($q->params);
-			return array('sql' => $q->getDebugQuery(), 'insertedid' => wire('database')->lastInsertId());
-		}
-	}
-
-	function makedsqlqueryinsert(atk4\dsql\Query $q, $insertvalues) {
-		foreach ($insertvalues as $key => $value) {
-			if (trim($value) != '') {
-				$q->set($key, $value);
-			}
-			
-		}
-		return $q;
+	function getstates() {
+		$sql = wire('database')->prepare("SELECT * FROM states");
+		$sql->execute();
+		return $sql->fetchAll(PDO::FETCH_ASSOC);
 	}
--- site/templates/product-page.php
+++ site/templates/product-page.php
@@ -1,78 +1,67 @@
 <?php include('./_head.php'); ?>
-
 	<div class="container page">
 		<div class="product-container">
 			<ol class="breadcrumb">
-				<li><a href="<?php echo $page->parent->parent->parent->url; ?>"><?php echo $page->parent->parent->parent->title; ?></a></li>
-			  <li><a href="<?php echo $page->parent->parent->url; ?>"><?php echo $page->parent->parent->title; ?></a></li>
-			  <li><a href="<?php echo $page->parent->url; ?>"><?php echo $page->parent->title; ?></a></li>
-			  <li>Spectrum - <?php echo $page->title ?> - <?php echo $page->product_features; ?></li>
-			 </ol>
-			<h1>Spectrum - <?php echo $page->title ?> - <?php echo $page->product_features; ?></h1>
+				<li><a href="<?= $page->parent->parent->parent->url; ?>"><?= $page->parent->parent->parent->title; ?></a></li>
+				<li><a href="<?= $page->parent->parent->url; ?>"><?= $page->parent->parent->title; ?></a></li>
+				<li><a href="<?= $page->parent->url; ?>"><?= $page->parent->title; ?></a></li>
+				<li>Spectrum - <?= $page->title ?> - <?= $page->product_features; ?></li>
+			</ol>
+			
+			<h1>Spectrum - <?= $page->title ?> - <?= $page->product_features; ?></h1>
 
 			<div class="row">
-			  <div class="col-md-5 col-sm-5">
-				<img class="product-img img-responsive" src="<?php echo $page->product_image->height(434)->url; ?>" alt="">
-				<h4 class="product-name">Spectrum - <?php echo $page->title ?> - <?php echo $page->product_features; ?></h4>
-				<p>Model: <?php echo $page->itemid; ?></p>
-				<!-- <p class="star">
-					<i class="fa fa-star" aria-hidden="true"></i>
-					<i class="fa fa-star" aria-hidden="true"></i>
-					<i class="fa fa-star" aria-hidden="true"></i>
-					<i class="fa fa-star" aria-hidden="true"></i>
-					<i class="fa fa-star-o" aria-hidden="true"></i>
-				</p> -->
-				<form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
-					<input type="hidden" name="action" value="add-to-cart">
-					<input type="hidden" name="itemID" value="<?php echo $page->itemid; ?>">
-					<input type="hidden" name="page" value="<?php echo $page->url; ?>">
-					<div class="quantity-group">
-						<label for="quantity">Quantity:</label>
-						<input class="quantity" type="text" name="qty" size="3">
-					</div>
-					<h3 class="price">$<?php echo $page->price; ?></h3>
-					<button class="btn btn-info add_to_cart" type="submit" name="add_to_cart">Add to Cart</button>
-				</form>
+				<div class="col-sm-5">
+					<img class="product-img img-responsive" src="<?= $page->product_image->height(434)->url; ?>" alt="">
+					<h4 class="product-name">Spectrum - <?=  $page->title ?> - <?= $page->product_features; ?></h4>
+					<p>Model: <?php echo $page->itemid; ?></p>
+					<form class="" action="<?php echo $pages->get('/cart/redir/')->url; ?>" method="post">
+						<input type="hidden" name="action" value="add-to-cart">
+						<input type="hidden" name="itemID" value="<?= $page->itemid; ?>">
+						<input type="hidden" name="page" value="<?= $page->url; ?>">
+						<div class="quantity-group">
+							<label for="quantity">Quantity:</label>
+							<input class="form-control input-sm qty" type="text" name="qty" size="3">
+						</div>
+						<h3 class="price">$<?php echo $page->price; ?></h3>
+						<button class="btn btn-info add_to_cart" type="submit" name="add_to_cart">Add to Cart</button>
+					</form>
 
 				</div>
-				<div class="bedroom-container col-sm-7">
+				<div class="col-sm-7">
 					<div class="bedroom-color img-responsive" style="background-color: <?= '#'.$page->itemid; ?>;">
 						<img class="bedroom img-responsive" src="<?php echo $config->urls->assets.'files/images/bedroom.png'; ?>">
 					</div>
 					<div class="description">
 						<h4>Product Features</h4>
-		                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis risus velit. Phasellus pellentesque
-		                    laoreet malesuada. Integer vel purus vel quam viverra suscipit at non risus. Cras luctus sodales metus,
-		                    malesuada pharetra mi tristique sed. Duis in pretium sapien. Aenean iaculis ante id est volutpat, quis
-		                    faucibus odio pretium. Quisque ut justo vitae leo semper fermentum. In vel nisi sed libero placerat egestas
-		                    in ac nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-		                    Nulla aliquet condimentum sem eu egestas. In hac habitasse platea dictumst. Orci varius natoque penatibus
-		                    et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus volutpat luctus. Integer pulvinar
-		                    malesuada turpis ut vestibulum.</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis risus velit. Phasellus pellentesque
+							laoreet malesuada. Integer vel purus vel quam viverra suscipit at non risus. Cras luctus sodales metus,
+							malesuada pharetra mi tristique sed. Duis in pretium sapien. Aenean iaculis ante id est volutpat, quis
+							faucibus odio pretium. Quisque ut justo vitae leo semper fermentum. In vel nisi sed libero placerat egestas
+							in ac nibh. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+							Nulla aliquet condimentum sem eu egestas. In hac habitasse platea dictumst. Orci varius natoque penatibus
+							et magnis dis parturient montes, nascetur ridiculus mus. Quisque faucibus volutpat luctus. Integer pulvinar
+							malesuada turpis ut vestibulum.
+						</p>
 					</div>
 				</div>
-
 			</div>
 		</div>
-		<!-- END PRODUCT CONTAINER -->
-		<div class="related-products">
-			<div class="row">
-				<div class="col-md-12">
-					<h3>Related Products</h3>
+	<!-- END PRODUCT CONTAINER -->
+	<div class="related-products">
+		<h3>Related Products</h3>
+		<div class="row">
+			<?php foreach ($page->siblings('limit=4', false) as $relate) : ?>
+				<div class="col-xs-6 col-sm-4 col-md-3 form-group">
+					<img class="img-responsive" src="<?php echo $relate->product_image->height(400)->url ?>" alt="">
+					<a href="<?php echo $relate->url; ?>"><h5><?php echo $relate->title; ?></h5></a>
+					<p><?php echo $relate->product_specifications; ?></p>
+					<a href="<?php echo $relate->url; ?>" class="btn btn-info">See More</a>
 				</div>
-			</div>
-			<div class="row">
-				<?php foreach ($page->siblings('limit=4', false) as $relate) : ?>
-					<div class="col-xs-6 col-sm-4 col-md-3 form-group">
-						<img class="img-responsive" src="<?php echo $relate->product_image->height(400)->url ?>" alt="">
-						<a href="<?php echo $relate->url; ?>"><h5><?php echo $relate->title; ?></h5></a>
-						<p><?php echo $relate->product_specifications; ?></p>
-						<a href="<?php echo $relate->url; ?>" class="btn btn-info">See More</a>
-					</div>
-				<?php endforeach; ?>
-			</div>
+			<?php endforeach; ?>
 		</div>
-		<!-- END RELATED PRODUCTS -->
-</div>
-
+	</div>
+	<!-- END RELATED PRODUCTS -->
+	</div>
 <?php include('./_foot.php'); // include footer markup ?>
