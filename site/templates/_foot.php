b4466652276e85b52ea9e1200c1cfaed2a4deda8
bbullemer <barbara.bullemer@gmail.com>
2017-08-25 15:03:59 -0500

    8/25/2017 v3
    changed footer styling

--- site/templates/_foot.php
+++ site/templates/_foot.php
@@ -9,7 +9,7 @@
         <footer class="hidden-print">
             <div class="container">
 				<div class="col-sm-4">
-                	<h5>Contact</h5>
+                	<h4>Contact</h4>
 					<p>
 						123 Main St<br>
 						Minneapolis, MN 55414
@@ -21,14 +21,14 @@
 
                 </div>
 				<div class="col-sm-4">
-                	<h5>Hours</h5>
+                	<h4>Hours</h4>
 					<p>
 						Monday - Friday&emsp;&emsp;&emsp;&nbsp;&nbsp;&nbsp;&nbsp;9:00AM - 8:00PM<br>
 						Saturday and Sunday&emsp;&nbsp;10:00AM - 6:00PM
 					</p><br>
                 </div>
 				<div class="col-sm-4 bottom">
-					<h5>Site Map</h5>
+					<h4>Site Map</h4>
 					<div class="row">
 						<div class="col-sm-6">
 							<ul>
