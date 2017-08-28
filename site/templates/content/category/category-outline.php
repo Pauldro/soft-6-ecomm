aa6da6bd0d9a6038c98bc44303a95a6a7b33bc6b
Paul Gomez <pauldro952@outlook.com>
2017-08-28 16:32:19 -0500

    Fixes for #5

--- site/templates/content/category/category-outline.php
+++ site/templates/content/category/category-outline.php
@@ -3,14 +3,9 @@
 	<?php foreach ($children as $child) : ?>
 		<div class="col-sm-4">
 			<h2><a href="<?= $child->url; ?>"><?= $child->title; ?></a></h2>
-			<?php
-			if ($child->title == 'Paints') {
-			?>
-				<a href="<?= $child->url; ?>"><img class="img-responsive" src="<?= $child->product_image->url; ?>" alt=""></a>
-			<?php
-			}
-			?>
-				<a href="<?= $child->url; ?>"><img class="img-responsive" src="<?= $child->schematicimage->url; ?>" alt=""></a>
+			<a href="<?= $child->url; ?>">
+				<img class="img-responsive" src="<?= $child->product_image->url; ?>" alt="">
+			</a>
 		</div>
 	<?php endforeach; ?>
 </div>
