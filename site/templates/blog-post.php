ecbde1c5cbec5e9d3d29691ac57dd55cf2cf4a12
bbullemer <barbara.bullemer@gmail.com>
2017-08-30 16:46:00 -0500

    Styling blog post page
    Changed responsive columns for blog post pages, added next/prev buttons for bottom of the page

--- site/templates/blog-post.php
+++ site/templates/blog-post.php
@@ -1,11 +1,29 @@
 <?php include('./_head.php'); ?>
 
-<div class="container page">
+<div class="blog-post container page">
 	<div class="col-sm-9">
 		        <h1><?php echo $page->title; ?></h1>
-		        <h4><?php echo $page->blog_date; ?></h4>
                 <img class='img-responsive' src='<?= $page->blog_image->height(400)->url; ?>' alt=''>
+				<h4><?php echo $page->blog_date; ?></h4>
                 <p><?= $page->blog_body; ?></p>
+	</div>
+	<?php
+		$prevLink = $page->prev->url;
+		$nextLink = $page->next->url;
+	?>
+	<div class="next col-sm-9">
+		<div class="row">
+			<div class="col-sm-6">
+				<!-- <h4 class="prev-post"> -->
+					<a href="<?php echo $prevLink; ?>" class="btn btn-info prev-post">Previous Post</a>
+				<!-- </h4> -->
+			</div>
+			<div class="col-sm-6">
+				<!-- <h4 class="next-post"> -->
+					<a href="<?php echo $nextLink; ?>" class="btn btn-info next-post">Next Post</a>
+				<!-- </h4> -->
+			</div>
+		</div>
 	</div>
 </div>
 
--- site/templates/blog.php
+++ site/templates/blog.php
@@ -1,33 +1,34 @@
 <?php include('./_head.php'); ?>
 
 <div class="container page">
-	<div class="row">
         <h1>Spectrum Blog</h1>
-        <hr>
+		<hr>
 		<?php
 		   $panels = $pages->find("has_parent=blog");
 		   $panels = $panels->reverse();
 		   foreach($panels as $panel) {
 		?>
 
-			<div class="panels">
+			<div class="panels row">
+				<div class="col-xs-12">
+					<h2><a href="<?php echo $panel->url; ?>"><?php echo $panel->title; ?></a></h2>
+				</div>
+				<div class="col-md-3">
+					<?php
+	       		         if($panel->blog_image){
+	       		            echo "<a href='{$panel->url}'><img class='img-responsive' src='{$panel->blog_image->height(500)->url}' alt=''></a>";
 
-		         <div class="content">
-		            <h2><a href="<?php echo $panel->url; ?>"><?php echo $panel->title; ?></a></h2>
-                    <?php
-           		         if($panel->blog_image){
-           		            echo "<a href='{$panel->url}'><img class='img-responsive' src='{$panel->blog_image->height(200)->url}' alt=''></a>";
-
-           		         }
-       		         ?>
-		            <h4><?php echo $panel->blog_date; ?></h4>
+	       		         }
+	   		         ?>
+				</div>
+	            <div class="col-md-6">
+					<h4><?php echo $panel->blog_date; ?></h4>
 		            <?php echo $panel->blog_description; ?>
 		            <p class="readmore"><a href="<?php echo $panel->url; ?>">Read More</a></p>
-		         </div>
-		      <hr>
-		      </div>
+	            </div>
+		   </div>
+		   <hr>
 		<?php } ?>
-	</div>
 </div>
 
 <?php include('./_foot.php'); // include footer markup ?>
