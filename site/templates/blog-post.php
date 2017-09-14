bf2a58dff6a9c2af76a14fcbfb0927dccdbf45dd
bbullemer <barbara.bullemer@gmail.com>
2017-09-14 11:34:23 -0500

    #10 blog-post.php fixed
    removed unneccessary variables;
    fixed indenting
    removed blog-post class

--- site/templates/blog-post.php
+++ site/templates/blog-post.php
@@ -1,27 +1,19 @@
 <?php include('./_head.php'); ?>
 
-<div class="blog-post container page">
+<div class="container page">
 	<div class="col-sm-9">
-		        <h1><?php echo $page->title; ?></h1>
-                <img class='img-responsive' src='<?= $page->blog_image->height(400)->url; ?>' alt=''>
-				<h4><?php echo $page->blog_date; ?></h4>
-                <p><?= $page->body; ?></p>
+        <h1 class="blog-post"><?php echo $page->title; ?></h1>
+        <img class='blog-post img-responsive' src='<?= $page->blog_image->height(400)->url; ?>' alt=''>
+		<h4 class="blog-post"><?php echo $page->blog_date; ?></h4>
+        <p><?= $page->body; ?></p>
 	</div>
-	<?php
-		$prevLink = $page->prev->url;
-		$nextLink = $page->next->url;
-	?>
 	<div class="next col-sm-9">
 		<div class="row">
 			<div class="col-sm-6">
-				<!-- <h4 class="prev-post"> -->
-					<a href="<?php echo $prevLink; ?>" class="btn btn-info prev-post">Previous Post</a>
-				<!-- </h4> -->
+				<a href="<?php echo $page->prev->url; ?>" class="btn btn-info prev-post">Previous Post</a>
 			</div>
 			<div class="col-sm-6">
-				<!-- <h4 class="next-post"> -->
-					<a href="<?php echo $nextLink; ?>" class="btn btn-info next-post">Next Post</a>
-				<!-- </h4> -->
+				<a href="<?php echo $page->next->url; ?>" class="btn btn-info next-post">Next Post</a>
 			</div>
 		</div>
 	</div>
