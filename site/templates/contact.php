3f581bb69a58592d8eaf99b90dea8c340c123db3
bbullemer <barbara.bullemer@gmail.com>
2017-09-21 12:49:37 -0500

    Fixes #22 Contact Page Map
    added mew field to contact page to hold google map source url

--- site/templates/contact.php
+++ site/templates/contact.php
@@ -37,9 +37,7 @@
 		</div>
 	</div>
 	<div class="row">
-		<iframe class="col-xs-12" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2832.4138103758974!2d-93.38800038446558!3d44.
-		772368379098815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f63d04cca1d973%3A0x2e24659c4a9ff01b!2s8628+
-		Eagle+Creek+Cir%2C+Savage%2C+MN+55378!5e0!3m2!1sen!2sus!4v1505404276308"
+		<iframe class="col-xs-12" src="<?= $page->google_maps; ?>"
 		width="1200" height="250" frameborder="0" style="border:0"></iframe>
 	</div>
 </div>
