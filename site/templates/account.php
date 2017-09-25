4090b4f01632789feca7a639ad733efab670a55e
bbullemer <barbara.bullemer@gmail.com>
2017-09-25 09:51:22 -0500

    Created Account Page
    Created account page for customer

---
+++ site/templates/account.php
@@ -0,0 +1,60 @@
+<?php include('./_head.php'); ?>
+
+<div class="container page">
+    <div class="row">
+        <div class="col-sm-12">
+            <h1>Welcome back, Customer!</h1><p>If you would like to login as a different user, or you would like to log out of your account, <a href="#">log out here.</a></p>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="panel panel-primary">
+                      <div class="panel-heading">
+                        <h3 class="panel-title">Customer Information</h3>
+                      </div>
+                      <div class="panel-body">
+                          <form class="form-horizontal">
+                              <div class="form-group">
+                                <label class="col-sm-4">Name</label>
+                                <p class="col-sm-8">John Snow</p>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-4">Email</label>
+                                  <p class="col-sm-8">customer@email.com</p>
+                              </div>
+                              <div class="form-group">
+                                  <label class="col-sm-4">Shipping Address</label>
+                                  <p class="col-sm-8">123 Main St.</br>
+                                    Minneapolis, MN 55408</p>
+                              </div>
+                          </form>
+                      </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="panel panel-info">
+                      <div class="panel-heading">
+                        <h3 class="panel-title">Login Information</h3>
+                      </div>
+                      <div class="panel-body">
+                         <p class="col-sm-8">Change your password</p>
+                         <p class="col-sm-8">Log out of your account</p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel panel-success">
+                      <div class="panel-heading">
+                        <h3 class="panel-title">Orders</h3>
+                      </div>
+                      <div class="panel-body">
+                         
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('./_foot.php'); // include footer markup ?>
