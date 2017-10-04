2bd80b2c42c7b0534ea2e152f7e3e768fdb9ddf0
bbullemer <barbara.bullemer@gmail.com>
2017-10-04 16:24:51 -0500

    About Page icons
    changed headline and color for icons

--- site/templates/content/about/icon.php
+++ site/templates/content/about/icon.php
@@ -3,10 +3,10 @@
 <?php $colors = $page->children; ?>
 <?php foreach ($colors as $color) : ?>
 
-    <div class="col-md-4" style="color: <?php echo $color->headline; ?>;">
+    <div class="col-md-4">
         <img src="<?php echo $color->images->height(250)->url; ?>" class="img-responsive center-block" alt="">
         <h2 style="text-align: center; color: <?php echo $color->headline; ?>;"><?php echo $color->title; ?></h2>
-        <p><?php echo $color->body; ?></p>
+        <h6 style="text-align: center; color: <?php echo $color->headline; ?>;"><?php echo $color->body; ?></h6>
     </div>
 
 <?php endforeach; ?>
