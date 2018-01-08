71b3911340ee365c820b08e6d5832dd1b5462a08
bbullemer <barbara.bullemer@gmail.com>
2018-01-08 08:19:35 -0600

    #34 Events Page
    - created main events page template to display all events
    - made up 3 fake events to test
    - created event page template

--- site/templates/event.php
+++ site/templates/event.php
@@ -1,7 +1,12 @@
 <?php include('./_head.php'); ?>
 
 <div class="container page">
-        <h1>Event Name</h1>
+    <h1><?php echo $page->title; ?></h1>
+    <hr>
+    <p><?php echo $page->startdate; ?> - <?php echo $page->throughdate; ?></p>
+    <p><?php echo $page->address; ?></p>
+    <p><a href="<?php echo $page->link; ?>"><?php echo $page->link; ?></a></p>
+    <p><?php echo $page->body; ?></p>
 </div>
 
 <?php include('./_foot.php'); ?>
--- site/templates/events.php
+++ site/templates/events.php
@@ -1,17 +1,20 @@
 <?php include('./_head.php'); ?>
 
 <div class="container page">
-        <h1>Spectrum Events</h1>
-		<hr>
-        <?php $panels = $pages->find("has_parent=event"); ?>
-        <?php $panels = $panels->reverse(); ?>
-        <?php foreach($panels as $panel) : ?>
-
-            <div class="panels row">
-           </div>
-           <hr>
-
-        <?php endforeach; ?>
+    <h1>Spectrum Events</h1>
+	<hr>
+    <?php $events = $pages->find("has_parent=events"); ?>
+    <?php $events = $events->reverse(); ?>
+    <?php foreach($events as $event) : ?>
+        <div class="row">
+            <div class="col-md-9">
+                <h2><a href="<?php echo $event->url; ?>"><?php echo $event->title; ?></a></h2>
+                <p><?php echo $event->startdate; ?> - <?php echo $event->throughdate; ?></p>
+                <p><?php echo $event->address; ?></p>
+            </div>
+        </div>
+        <hr>
+    <?php endforeach; ?>
 </div>
 
 <?php include('./_foot.php'); ?>
